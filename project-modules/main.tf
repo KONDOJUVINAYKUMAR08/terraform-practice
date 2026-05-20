b155571becb0eab0e596c94d92e9dd2d9fcb0d01
Vinay <kondojuvinaykumar2004@gmail.com>
2026-05-21 00:02:57 +0530

    Modules (Local-modules) in terraform

---
+++ project-modules/main.tf
@@ -0,0 +1,32 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "registry.terraform.io/hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+module "dev_vpc" {
+    source = "./modules/vpc"
+
+    vpc_name = "dev"
+    vpc_cidr = "10.0.0.0/16"
+    public_subnet_cidr = "10.0.1.0/24"
+    private_subnet_cidr = "10.0.2.0/24"
+    environment = "dev"
+}
+
+module "prod_vpc" {
+    source = "./modules/vpc"
+
+    vpc_name = "prod"
+    vpc_cidr = "10.10.0.0/16"
+    public_subnet_cidr = "10.10.1.0/24"
+    private_subnet_cidr = "10.10.2.0/24"
+    environment = "prod"
+}
